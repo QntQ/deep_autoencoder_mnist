82ed3b256758e8f0aae6f5393f3972ee4b0f2c34
Fynn <f.madrian18230@hs-ansbach.de>
2023-02-23 16:47:33 +0100

    adds training

--- Autoencoder.py
+++ Autoencoder.py
@@ -1,20 +1,46 @@
-from keras.layers import Input, Dense
-from keras.models import Model
+from tensorflow.keras.utils import plot_model  # for plotting model diagram
+# for adding layers to DAE model
+from keras.layers import Dense, LeakyReLU, BatchNormalization, Input, Dense
+from keras import Input  # for instantiating a keras tensor
+from keras.models import Model  # for creating a Neural Network Autoencoder model
+
+
+from tensorflow import keras  # for building Neural Networks
+print('Tensorflow/Keras: %s' % keras.__version__)  # print version
 
 
 def create_autoencoder():
-    input_img = Input(shape=(784,))
-    encoded = Dense(128, activation="relu")(input_img)
-    encoded = Dense(64, activation="relu")(encoded)
-    encoded = Dense(32, activation="relu")(encoded)
-    decoded = Dense(64, activation="relu")(encoded)
-    decoded = Dense(128, activation="relu")(decoded)
-    decoded = Dense(784, activation="linear")(decoded)
-    # this model maps an input to its reconstruction
-    autoencoder = Model(input_img, decoded)
-    autoencoder.summary()
-    autoencoder.compile(optimizer="adam", loss="mean_squared_error")
-    return autoencoder
+
+    n_inputs = 784
+    # --- Input Layer
+    # Specify input shape
+    visible = Input(shape=(n_inputs,), name='Input-Layer')
+
+    # --- Encoder Layer
+    e = Dense(units=n_inputs, name='Encoder-Layer')(visible)
+    e = BatchNormalization(name='Encoder-Layer-Normalization')(e)
+    e = LeakyReLU(name='Encoder-Layer-Activation')(e)
+
+    # --- Middle Layer
+    middle = Dense(units=n_inputs, activation='linear', activity_regularizer=keras.regularizers.L1(
+        0.0001), name='Middle-Hidden-Layer')(e)
+
+    # --- Decoder Layer
+    d = Dense(units=n_inputs, name='Decoder-Layer')(middle)
+    d = BatchNormalization(name='Decoder-Layer-Normalization')(d)
+    d = LeakyReLU(name='Decoder-Layer-Activation')(d)
+
+    # --- Output layer
+    output = Dense(units=n_inputs, activation='sigmoid',
+                   name='Output-Layer')(d)
+
+    # Define denoising autoencoder model
+    model = Model(inputs=visible, outputs=output,
+                  name='Denoising-Autoencoder-Model')
+
+    # Compile denoising autoencoder model
+    model.compile(optimizer='adam', loss='mse')
+    return model
 
 
 def train_autoencoder(autoencoder, x_train, x_train_noise, x_test, x_test_noise, epochs=1000, batch_size=128):
