1973b95f4baef69ed891eb8d0442f40bdb242bb4
fynnm <fynn.madrian@outlook.com>
2023-03-02 12:21:09 +0100

    finished training

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     training = training[:60000].reshape(60000, 784)
     testing = testing[:3000].reshape(3000, 784)
 
-    training_noise = apply_noise_to_data(training, 0.2)
-    testing_noise = apply_noise_to_data(testing, 0.2)
+    training_noise = apply_noise_to_data(training, 0.2,True)
+    testing_noise = apply_noise_to_data(testing, 0.2,True)
 
     if os.path.exists("autoencoder.h5"):
         model = keras.models.load_model("autoencoder.h5")
@@ -23,12 +23,12 @@ if __name__ == "__main__":
     else:
         model = create_autoencoder()
         print("created model")
-        model = train_autoencoder(
-            model, training_noise, training, testing_noise, testing, epochs=200, batch_size=16)
-        print("trained model")
+    model = train_autoencoder(
+        model, training_noise, training, testing_noise, testing, epochs=200, batch_size=16)
+    print("trained model")
     model.save("autoencoder.h5")
     print("saved model")
     print("evaluating model")
 
-    evaluate(model, testing, testing_noise, num_output=10)
+    evaluate(model, testing, testing_noise, num_output=100)
     #best: 0.003210775055032928
